0dc4870f2c4d2e809d7db3a749e8567daa8ff508
Timuuurkaaa <husnytdinov34@gmail.com>
2024-10-21 12:24:32 +0500

    Добавьте файлы проекта.

---
+++ ServicePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Husnutdinov_Autoservice
+{
+    /// <summary>
+    /// Логика взаимодействия для ServicePage.xaml
+    /// </summary>
+    public partial class ServicePage : Page
+    {
+        public ServicePage()
+        {
+            InitializeComponent();
+            //добавляем строки
+            // загрузить в список из бд
+            var currentService = Husnutdinov_autoserviceEntities.GetContext().Service.ToList();
+            //связать с нашим листвью
+            ServiceListView.ItemsSource = currentService;
+            //добавили строки
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+    }
+}
