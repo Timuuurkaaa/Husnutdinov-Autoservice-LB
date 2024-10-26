102b0b01a0b88d5d5afd09edb25c0ce13f127f4e
Timuuurkaaa <husnytdinov34@gmail.com>
2024-10-26 12:50:57 +0500

    ЛАБА 7

--- ServicePage.xaml.cs
+++ ServicePage.xaml.cs
@@ -20,6 +20,11 @@ namespace Husnutdinov_Autoservice
     /// </summary>
     public partial class ServicePage : Page
     {
+        int CountRecords;//Количество записей в таблице
+        int CountPage;//Общее количество страниц
+        int CurrentPage = 0;//Текущая страница
+        List<Service> CurrentPageList = new List<Service>();
+        List<Service> TableList;
         public ServicePage()
         {
             InitializeComponent();
@@ -43,27 +48,27 @@ namespace Husnutdinov_Autoservice
             //фильтрация по условию задания
             if (ComboType.SelectedIndex == 0)
             {
-                currentServices = currentServices.Where(p => (Convert.ToInt32(p.Discount) >= 0 && Convert.ToInt32(p.Discount) <= 100)).ToList();
+                currentServices = currentServices.Where(p => ((p.DiscountInt) >= 0 && (p.DiscountInt) <= 100)).ToList();
             }
             if (ComboType.SelectedIndex == 1)
             {
-                currentServices = currentServices.Where(p => (Convert.ToInt32(p.Discount) >= 0 && Convert.ToInt32(p.Discount) < 5)).ToList();
+                currentServices = currentServices.Where(p => ((p.DiscountInt) >= 0 && (p.DiscountInt) < 5)).ToList();
             }
             if (ComboType.SelectedIndex == 2)
             {
-                currentServices = currentServices.Where(p => (Convert.ToInt32(p.Discount) >= 5 && Convert.ToInt32(p.Discount) < 15)).ToList();
+                currentServices = currentServices.Where(p => ((p.DiscountInt) >= 5 && (p.DiscountInt) < 15)).ToList();
             }
             if (ComboType.SelectedIndex == 3)
             {
-                currentServices = currentServices.Where(p => (Convert.ToInt32(p.Discount) >= 15 && Convert.ToInt32(p.Discount) < 30)).ToList();
+                currentServices = currentServices.Where(p => ((p.DiscountInt) >= 15 && (p.DiscountInt) < 30)).ToList();
             }
             if (ComboType.SelectedIndex == 4)
             {
-                currentServices = currentServices.Where(p => (Convert.ToInt32(p.Discount) >= 30 && Convert.ToInt32(p.Discount) < 70)).ToList();
+                currentServices = currentServices.Where(p => ((p.DiscountInt) >= 30 && (p.DiscountInt) < 70)).ToList();
             }
             if (ComboType.SelectedIndex == 5)
             {
-                currentServices = currentServices.Where(p => (Convert.ToInt32(p.Discount) >= 70 && Convert.ToInt32(p.Discount) < 100)).ToList();
+                currentServices = currentServices.Where(p => ((p.DiscountInt) >= 70 && (p.DiscountInt) < 100)).ToList();
             }
             //реализуем поиск данных в листвью при вводе текста в окно поиска
             currentServices = currentServices.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
@@ -74,16 +79,24 @@ namespace Husnutdinov_Autoservice
             if (RButtonDown.IsChecked.Value)
             {
                 //для отображения итогов фильтра и поиска листвью по убыванию
-                ServiceListView.ItemsSource = currentServices.OrderByDescending(p => p.Cost).ToList();
+                currentServices = currentServices.OrderByDescending(p => p.Cost).ToList();
             }
 
             if (RButtonUp.IsChecked.Value)
             {
                 //для отображения итогов фильтра и поиска листвью по возрастанию
-                ServiceListView.ItemsSource = currentServices.OrderBy(p => p.Cost).ToList();
+                currentServices = currentServices.OrderBy(p => p.Cost).ToList();
             }
+            //для отображения итогов фильтра и поиска в листвью
+            ServiceListView.ItemsSource = currentServices;
+            //заполнение таблицы для постраничного вывода
+            TableList = currentServices;
+            //вызов функции отображения кол-ва страниц с параметрами:
+            //направление 0 - начальная загрузка
+            //0 - выбранная страница
+            ChangePage(0, 0);
         }
-
+        
         private void Button_Click(object sender, RoutedEventArgs e)
         {
         }
@@ -126,12 +139,153 @@ namespace Husnutdinov_Autoservice
                 Husnutdinov_autoserviceEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
                 ServiceListView.ItemsSource = Husnutdinov_autoserviceEntities.GetContext().Service.ToList();
             }
+            UpdateSerices();
         }
 
         private void EditButton_Click(object sender, RoutedEventArgs e)
         {
             //открыть окно ред/доб услуг
             Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Service));
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            //забираем Сервис, для которого нажата кнопка "Удалить"
+            var currentService = (sender as Button).DataContext as Service;
+
+            //проверка на возможность удаления
+            var currentClientServices = Husnutdinov_autoserviceEntities.GetContext().ClientService.ToList();
+            currentClientServices = currentClientServices.Where(p => p.ServiceID == currentService.ID).ToList();
+            if (currentClientServices.Count != 0) //если есть записи на этот сервис
+            {
+                MessageBox.Show("Невозможно выполнить удаление, так как существуют записи на эту услугу");
+            }
+            else
+            {
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        Husnutdinov_autoserviceEntities.GetContext().Service.Remove(currentService);
+                        Husnutdinov_autoserviceEntities.GetContext().SaveChanges();
+                        //выводим в листвью измененную таблицу Сервис
+                        ServiceListView.ItemsSource = Husnutdinov_autoserviceEntities.GetContext().Service.ToList();
+                        // чтобы применились фильтры и поиск, если они были на форме изначально
+                        UpdateSerices();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+            }
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+        }
+        private void ChangePage(int direction, int? selectedPage)//Функция отвечающая за разделение list'a
+        {
+            //direction - направление. 0 - начало, 1 - предыдущая страница, 2 - следующая страница
+            //selectedPage - при нажатии на стрелочки передается null,
+            //при выборе определенной страницы в этой переменной находится номер страницы
+            CurrentPageList.Clear();// начальная очистка листа
+            CountRecords = TableList.Count;//определение количества записей во всем списке
+            //определение кол-ва страниц
+            if (CountRecords % 10 > 0)
+            {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else
+            {
+                CountPage = CountRecords / 10;
+            }
+            Boolean Ifupdate = true;
+            //Проверка на правильность - если
+            //CurrentPage(номер текущей страница) "правильный"
+
+            int min;
+            if (selectedPage.HasValue)//Проверка на значение не null (тк может быть null)
+            {
+                if (selectedPage >= 0 && selectedPage <= CountPage)
+                {
+                    CurrentPage = (int)selectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    for (int i = CurrentPage * 10; i < min; i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else //если нажата стрелка
+            {
+                switch (direction)
+                {
+                    case 1://нажата кнопка "Предыдущая страница"
+                        if (CurrentPage > 0)
+                //то есть кнопка нажата правильно и "назад" можно идти
+                        {
+                            CurrentPage--;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                            //в случае если currentPage попытается выйти из диапазона внесение данных не произойдет
+                        }
+                        break;
+                    case 2://нажата кнопка "следующая страница"
+                        if (CurrentPage < CountPage - 1)
+                        //если вперед идти можно
+                        {
+                            CurrentPage++;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+                }
+            }
+            if (Ifupdate)//если currentPage не вышел из диапазона, то
+            {
+                PageListBox.Items.Clear();
+                //Удаление старых значений из listBox'а номеров страниц, нужно чтобы при изменении
+                //кол-ва записей кол-во страниц динамически изменялось
+                for (int i = 1; i <= CountPage; i++)
+                {
+                    PageListBox.Items.Add(i);
+                }
+                PageListBox.SelectedIndex = CurrentPage;
+
+                //вывод количества записей на странице и общего количества
+                min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                TBCount.Text = min.ToString();
+                TBAllRecords.Text = " из " + CountRecords.ToString();
+
+                ServiceListView.ItemsSource = CurrentPageList;
+                //обновить отображение списка услуг
+                ServiceListView.Items.Refresh();
+            }
+        }
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
         }
     }
 }
